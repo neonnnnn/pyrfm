3b5ec10c584e47ea5f059007b711b60628901bcc
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2018-03-08 11:09:59 +0900

    add h01 heuristics and bias term for poly kernel

--- randfeature/random_maclaurin.py
+++ randfeature/random_maclaurin.py
@@ -23,17 +23,19 @@ def _make_projection_matrices(i_hash, b_hash, D):
 
 
 class RandomMaclaurin(BaseEstimator, TransformerMixin):
-    def __init__(self, D, p, kernel='poly', degree=2, gamma='auto', coefs=None, random_state=1,
-                 max_n=50):
+    def __init__(self, D, p, kernel='poly', degree=2, gamma='auto', bias=0, coefs=None, 
+                 random_state=1, max_n=50, h01=False):
         self.D = D
         self.p = p
         self.gamma = gamma
         self.degree = degree
         self.coefs = coefs
+        self.bias = bias
         self.kernel = kernel
         self.random_state = check_random_state(random_state)
         self.max_n = max_n
         self.p_choice = None
+        self.h01 = h01
 
     def fit(self, X, y=None):
         d = check_array(X, True).shape[1]
@@ -45,14 +47,18 @@ class RandomMaclaurin(BaseEstimator, TransformerMixin):
 
         if self.coefs is None:
             if self.kernel == 'poly':
-                self.coefs = np.zeros(self.degree+1)
-                self.coefs[-1] = 1
+                self.coefs = self.bias ** np.arange(self.degree+1)[::-1]
+                self.coefs *= binom(self.degree, range(degree+1))
+                self.coefs /= np.factorial(self.degree+1)
                 coefs = self.coefs
             elif self.kernel == 'exp':
                 coefs = self._gamma ** np.arange(self.max_n)
                 coefs /= factorial(range(self.max_n))
         else:
             coefs = self.coefs
+
+        if self.h01:
+            coefs[0, 1] = 0.
 
         if self.p_choice is None:
             p_choice = (1/self.p) ** (np.arange(len(self.coefs)) + 1)
@@ -82,5 +88,7 @@ class RandomMaclaurin(BaseEstimator, TransformerMixin):
 
         output *= np.sqrt(self.coefs[self.orders]/self.D)
         output /= np.sqrt(self.p_choice[self.orders])
-
+        if self.h01:
+            output = np.hstack([raw_X, output])
+            output = np.hstack((np.ones(n, 1), output))
         return output
