a46510805172b45dbb80b94417a18c7c0bdf3743
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2018-03-12 00:58:23 +0900

    add random kernel product map

--- randfeature/__init__.py
+++ randfeature/__init__.py
@@ -1,3 +1,5 @@
 from .tensor_sketch import TensorSketch
 from .random_maclaurin import RandomMaclaurin
 from .random_fourier import RandomFourier
+from .random_kernel_product import RandomKernelProduct
+from .kernel_fast import all_subset, anova
