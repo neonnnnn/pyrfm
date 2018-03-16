e13a0d2fe382353d83ffac28f756867d7e91ef10
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2018-03-16 11:05:19 +0900

    fix a bit

--- randfeature/random_kernel.py
+++ randfeature/random_kernel.py
@@ -4,7 +4,7 @@ from sklearn.utils import check_random_state, check_array
 from sklearn.utils.extmath import safe_sparse_dot
 from scipy.sparse import issparse
 from math import sqrt
-from .kernel_fast import anova, all_subset
+from .kernels import anova, all_subset
 
 
 def _anova(degree=2):
@@ -13,29 +13,49 @@ def _anova(degree=2):
 
     return __anova
 
+def dot():
+    def _dot(X, Y):
+        return safe_sparse_dot(X, Y.T, True)
 
-class RandomKernelProduct(BaseEstimator, TransformerMixin):
+    return _dot
+
+
+class RandomKernel(BaseEstimator, TransformerMixin):
     def __init__(self, D=100, kernel='anova', degree=2, random_state=None):
         self.D = D
         self.degree = degree
         self.random_state = random_state
-
-        if isinstance(kernel, str):
-            if kernel == 'anova':
-                self.kernel = _anova(self.degree)
-            elif kernel == 'all_subset':
-                self.kernel = all_subset
-        else:
-            self.kernel = kernel
+        self.kernel = kernel
 
     def fit(self, X, y=None):
-        random_state = check_random_state(self.andom_state)
+        random_state = check_random_state(self.random_state)
         d = check_array(X, ['csr']).shape[1]
-        self.Projs = random_state.randint(2, size=(self.D, d))*2-1
+        if isinstance(self.kernel, str):
+            if self.kernel == 'anova':
+                self._kernel = _anova(self.degree)
+            elif self.kernel == 'all_subset':
+                self._kernel = all_subset
+            elif self.kernel in ['dot', 'poly']:
+                self._kerel = dot
+        else:
+            self._kernel = kernel
+
+        if self.kernel == 'poly':
+            self.projs = [random_state.randint(2, size=(self.D, d))*2-1
+                          for _ in range(self.degree)]
+        else:
+            self.Projs = random_state.randint(2, size=(self.D, d))*2-1
+
         return self
 
     def transform(self, raw_X):
         n, d = check_array(raw_X, ['csr']).shape
-        output = self.kernel(raw_X, self.Projs).astype(np.float64)
+        if self.kernel != 'poly':
+            output = self._kernel(raw_X, self.Projs).astype(np.float64)
+        else:
+            output = self._kernel(raw_X, self.Pros[0]).astype(np.float64)
+            for proj in self.Projs[1:]:
+                output *= self._kernel(raw_X, proj).astype(np.float64)
+
         output /= sqrt(self.D)
         return output
