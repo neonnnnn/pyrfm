Fix some bugs and add normalization argument

pyrfm/linear_model/adagrad.py
@@ -3,38 +3,131 @@ import warnings
 
 import numpy as np
 from scipy import sparse
-
-
 from sklearn.utils.extmath import safe_sparse_dot
 from sklearn.utils import check_X_y, check_random_state
 
-from .loss import Squared, SquaredHinge, Logistic, Hinge
+from .loss_fast import Squared, SquaredHinge, Logistic, Hinge
 from .base import BaseLinear, LinearClassifierMixin, LinearRegressorMixin
 from sklearn.kernel_approximation import RBFSampler
+from .adagrad_fast import _adagrad_fast
+from sklearn.utils.validation import check_is_fitted
 
 
 class BaseAdaGradEstimator(BaseLinear):
     """AdaGrad solver for linear models with random feature maps.
-    Random feature mapping is computed just before computing prediction for
-    each sample.
+    Random feature mapping is computed just before computing prediction and
+    gradient.
+
+    Parameters
+    ----------
+    transformer : scikit-learn Transformer object, default=RBFSampler()
+        A scikit-learn TransformerMixin object.
+        transformer must have (1) n_components attribute, (2) fit(X, y),
+        and (3) transform(X).
+
+    eta : double, default=1.0
+        Step-size parameter.
+
+    loss : str
+        Which loss function to use. Following losses can be used:
+            'squared' (for regression)
+            'squared_hinge' (for classification)
+            'hinge' (for classification)
+            'logistic' (for classification)
+
+    C : double, default=1.0
+        Weight of loss term.
+
+    alpha : double, default=1.0
+        Weight of the penalty term.
+
+    l1_ratio : double, default=0
+        Ratio of L1 regularizer.
+        Weight of L1 regularizer is alpha * l1_ratio and that of L2 regularizer
+        is 0.5 * alpha * (1-l1_ratio).
+        If l1_ratio = 0 : Ridge.
+        else If l1_ratio = 1 : Lasso.
+        else : Elastic Net.
+
+    normalize : bool, default=False
+        Whether normalize random features or not.
+        If true, the adagrad solver computes running mean and variance
+        at learning, and uses them for inference.
+
+    fit_intercept : bool, default=True
+        Whether to fit intercept (bias term) or not.
+
+    max_iter : int
+        Maximum number of iterations.
+
+    tol : double
+        Tolerance of stopping criterion.
+        If sum of absolute val of update in one epoch is lower than tol,
+        the AdaGrad solver stops learning.
+
+    eps : double
+        A small double to avoid zero-division.
+
+    warm_start : bool
+        Whether to activate warm-start or not.
+
+    random_state : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+
+    verbose : bool, default=True
+        Verbose mode or not.
+
+
+    Attributes
+    ----------
+    self.coef_ : array, shape (n_components, )
+        The learned coefficients of the linear model.
+
+    self.intercept_ : array, shape (1, )
+        The learned intercept (bias) of the linear model.
+
+    self.acc_grad_, self.acc_grad_norm_ : array, shape (n_components, )
+        The sum of gradients and sum of norm of gradient for coefficients.
+        They are used in adagrad.
+
+    self.acc_grad_intercept_, self.acc_grad_intercept_norm :
+     array, shape (n_components, )
+        The sum of gradients and sum of norm of gradient for intercept_.
+        They are used in adagrad.
+
+    self.mean_, self.var_ : array or None, shape (n_components, )
+        The running mean and variances of random feature vectors.
+        They are used if normalize=True (they are None if False).
+
+    References
+    ---------
+    [1] Adaptive Subgradient Methods for Online Learning and Stochastic
+    Optimization.
+    Jonh Duchi, Elad Hazan, and Yoram Singer.
+    JMLR 2011 (vol 12), pp. 2121--2159.
     """
     LOSSES = {
         'squared': Squared(),
-        'suqared_hinge': SquaredHinge(),
+        'squared_hinge': SquaredHinge(),
         'logistic': Logistic(),
         'hinge': Hinge()
     }
 
     def __init__(self, transformer=RBFSampler(), eta=1.0, loss='squared_hinge',
-                 penalty='l2', C=1.0, alpha=1.0, fit_intercept=True,
-                 max_iter=100, tol=1e-6,  eps=1e-6, warm_start=False,
-                 random_state=None, verbose=True):
+                 C=1.0, alpha=1.0, l1_ratio=0, normalize=False,
+                 fit_intercept=True, max_iter=100, tol=1e-6,  eps=1e-6,
+                 warm_start=False, random_state=None, verbose=True):
         self.transformer = transformer
+        self.transformer_ = transformer
         self.eta = eta
         self.loss = loss
-        self.penalty = penalty
         self.C = C
         self.alpha = alpha
+        self.l1_ratio = l1_ratio
+        self.normalize = normalize
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
         self.tol = tol
@@ -43,109 +136,100 @@ class BaseAdaGradEstimator(BaseLinear):
         self.random_state = random_state
         self.verbose = verbose
 
+    def _predict(self, X):
+        check_is_fitted(self, "coef_")
+        y_pred = np.zeros((X.shape[0], ))
+        is_sparse = sparse.issparse(X)
+        for i, xi in enumerate(X):
+            if is_sparse:
+                xi_trans = self.transformer.transform(xi).ravel()
+            else:
+                xi_trans = self.transformer.transform(np.atleast_2d(xi)).ravel()
+
+            if self.normalize:
+                xi_trans = (xi_trans - self.mean_) / np.sqrt(self.var_)
+            y_pred[i] = safe_sparse_dot(xi_trans, self.coef_)
+            y_pred[i] += self.intercept_
+
+        return y_pred
+
     def fit(self, X, y):
         X, y = self._check_X_y(X, y)
         if not self.warm_start:
             self.transformer.fit(X)
 
         n_samples, n_features = X.shape
-        if not hasattr(self.transformer, 'n_components_actual_'):
+        if not (hasattr(self.transformer, 'n_components_actual_')):
             n_components = self.transformer.n_components
         else:
             n_components = self.transformer.n_components_actual_
 
-        if not self.warm_start and hasattr(self, 'coef_'):
+        if not (self.warm_start and hasattr(self, 'coef_')):
             self.coef_ = np.zeros(n_components)
 
-        if not self.warm_start and hasattr(self, 'intercept_'):
-            self.intercept_ = 0.
+        if not (self.warm_start and hasattr(self, 'intercept_')):
+            self.intercept_ = np.zeros((1,) )
 
-        if not self.warm_start and hasattr(self, 'acc_grad_'):
+        if not (self.warm_start and hasattr(self, 'acc_grad_')):
             self.acc_grad_ = np.zeros(n_components)
 
-        if not self.warm_start and hasattr(self, 'acc_grad_norm_'):
+        if not (self.warm_start and hasattr(self, 'acc_grad_norm_')):
             self.acc_grad_norm_ = np.zeros(n_components)
 
-        if self.fit_intercept:
-            if not self.warm_start and hasattr(self, 'acc_grad_intercept_'):
-                self.acc_grad_intercept_ = 0.
+        if not (self.warm_start and hasattr(self, 'acc_grad_intercept_')):
+            self.acc_grad_intercept_ = np.zeros(self.intercept_.shape)
 
-            if not self.warm_start and hasattr(self, 'acc_grad_norm_intercept_'):
-                self.acc_grad_norm_intercept_ = 0.
+        if not (self.warm_start
+                and hasattr(self, 'acc_grad_norm_intercept_')):
+            self.acc_grad_norm_intercept_ = np.zeros(self.intercept_.shape)
 
-        if not self.warm_start and hasattr(self, 't_'):
+        if not (self.warm_start and hasattr(self, 't_')):
             self.t_ = 1
 
         if self.loss not in self.LOSSES:
-            raise ValueError("loss {} is not supported.")
+            raise ValueError("loss {} is not supported.".format(self.loss))
 
+        if self.normalize:
+            if not (self.warm_start and hasattr(self, 'mean_')):
+                self.mean_ = np.zeros((n_components, ))
+
+            if not (self.warm_start and hasattr(self, 'var_')):
+                self.var_ = np.zeros((n_components,))
+        else:
+            self.mean_ = None
+            self.var_ = None
         loss = self.LOSSES[self.loss]
         alpha = self.alpha / self.C
         random_state = check_random_state(self.random_state)
 
         is_sparse = sparse.issparse(X)
-        for it in range(self.max_iter):
-            viol = 0
-            for i in random_state.permutation(n_samples):
-                if is_sparse:
-                    x = self.transformer.transform(X[i])
-                else:
-                    x = self.transformer.transform(np.atleast_2d(X[i]))
-                y_pred = safe_sparse_dot(x, self.coef_, True)
-                if self.fit_intercept:
-                    y_pred += self.intercept_
+        it = _adagrad_fast(self.coef_, self.intercept_, X, y, self.acc_grad_,
+                           self.acc_grad_norm_,  self.acc_grad_intercept_,
+                           self.acc_grad_norm_intercept_, self.mean_, self.var_,
+                           loss, alpha, self.l1_ratio, self.eta, self.t_,
+                           self.max_iter, self.tol, self.eps, is_sparse,
+                           self.verbose, self.fit_intercept, random_state,
+                           self.transformer)
+        self.t_ += n_samples*(it+1)
 
-                dloss = loss.dloss(y_pred, y[i])
-                if dloss != 0:
-                    grad = dloss * x.ravel()
-                    self.acc_grad_ += grad
-                    self.acc_grad_norm_ += grad**2
-                eta_t = self.eta * self.t_
-                denom = np.sqrt(self.acc_grad_norm_) + self.eps
-                denom += alpha * eta_t
-                coef_new = eta_t * (-self.acc_grad_ / self.t_) / denom
-                viol += np.sum(np.abs(coef_new-self.coef_))
-                self.coef_ = coef_new
-
-                if self.fit_intercept:
-                    self.acc_grad_intercept_ += dloss
-                    self.acc_grad_norm_ += dloss*dloss
-                    denom = np.sqrt(self.acc_grad_norm_intercept_) + self.eps
-                    intercept_new = - eta_t * self.acc_grad_norm_intercept_
-                    intercept_new /= self.t_ * denom
-                    viol += np.abs(intercept_new - self.intercept_)
-                    self.intercept_ = intercept_new
-
-                self.t_ += 1
-
-            if self.verbose:
-                print("Iteration {} Violation {}".format(it, viol))
-
-            if viol < self.tol:
-                if self.verbose:
-                    print("Converged at iteration {}".format(it))
-                break
         return self
 
 
 class AdaGradClassifier(BaseAdaGradEstimator, LinearClassifierMixin):
     LOSSES = {
-        'suqared_hinge': SquaredHinge(),
+        'squared_hinge': SquaredHinge(),
         'logistic': Logistic(),
         'hinge': Hinge()
     }
 
     def __init__(self, transformer=RBFSampler(), eta=1.0, loss='squared_hinge',
-                 penalty='l2', C=1.0, alpha=1.0, fit_intercept=True,
-                 max_iter=100, tol=1e-6, eps=1e-4, warm_start=False,
-                 random_state=None, verbose=True):
+                 C=1.0, alpha=1.0, l1_ratio=0., normalize=True,
+                 fit_intercept=True, max_iter=100, tol=1e-6, eps=1e-4,
+                 warm_start=False, random_state=None, verbose=True):
         super(AdaGradClassifier, self).__init__(
-            transformer, eta, loss, penalty, C, alpha, fit_intercept,
-            max_iter, tol, eps, warm_start, random_state, verbose
+            transformer, eta, loss, C, alpha, l1_ratio, normalize,
+            fit_intercept, max_iter, tol, eps, warm_start, random_state, verbose
         )
-
-    def _check_X_y(self, X, y):
-        return check_X_y(X, y, True, multi_output=False, y_numeric=False)
 
 
 class AdaGradRegressor(BaseAdaGradEstimator, LinearRegressorMixin):
@@ -154,13 +238,10 @@ class AdaGradRegressor(BaseAdaGradEstimator, LinearRegressorMixin):
     }
 
     def __init__(self, transformer=RBFSampler(), eta=1.0, loss='squared',
-                 penalty='l2', C=1.0, alpha=1.0, fit_intercept=True,
-                 max_iter=100, tol=1e-6, eps=1e-4, warm_start=False,
-                 random_state=None, verbose=True):
+                 C=1.0, alpha=1.0, l1_ratio=0., normalize=True,
+                 fit_intercept=True, max_iter=100, tol=1e-6, eps=1e-4,
+                 warm_start=False, random_state=None, verbose=True):
         super(AdaGradRegressor, self).__init__(
-            transformer, eta, loss, penalty, C, alpha, fit_intercept,
-            max_iter, tol, eps, warm_start, random_state, verbose
+            transformer, eta, loss, C, alpha, l1_ratio, normalize,
+            fit_intercept, max_iter, tol, eps, warm_start, random_state, verbose
         )
-
-    def _check_X_y(self, X, y):
-        return check_X_y(X, y, True, multi_output=False, y_numeric=True)