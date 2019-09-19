347728e613ef796a7fd3c8c275a35e9bc9ea8b27
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-09-19 19:35:27 +0900

    Fix for fastfood, random_projection, compact_random_feature, subsampled_hadamard, signed_circurant_random_matrix

--- pyrfm/__init__.py
+++ pyrfm/__init__.py
@@ -1,6 +1,8 @@
 from .random_feature import (RandomSubsetKernel, RandomMaclaurin, RandomKernel,
                              RandomFourier, SignedCirculantRandomKernel,
-                             TensorSketch, MB, SparseMB)
+                             TensorSketch, MB, SparseMB, RandomProjection,
+                             FastFood, SubsampledRandomHadamard,
+                             SignedCirculantRandomMatrix, CompactRandomFeature)
 from .kernels import (anova, all_subsets, chi_square, hellinger,
                       intersection, anova_fast)
 from .kernels_fast import _anova, _all_subsets, _chi_square, _intersection
--- pyrfm/random_feature/__init__.py
+++ pyrfm/random_feature/__init__.py
@@ -4,3 +4,13 @@ from .random_maclaurin import RandomMaclaurin
 from .signed_circulant_random_kernel import SignedCirculantRandomKernel
 from .maji_berg import MB, SparseMB
 from .tensor_sketch import TensorSketch
+from .compact_random_feature import CompactRandomFeature
+# for structured random features
+from .fastfood import FastFood
+from .subsampled_random_hadamard import SubsampledRandomHadamard
+from .signed_circulant_random_projection import SignedCirculantRandomMatrix
+from .random_projection import RandomProjection
+
+
+DOWNSAMPLES = {'srht': SubsampledRandomHadamard,
+               None: RandomProjection}
