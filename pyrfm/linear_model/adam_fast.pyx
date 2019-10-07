93d68de7346a019c609b13e2bb555f7754fed212
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-10-07 19:23:38 +0900

    Fix a bit

--- pyrfm/linear_model/adam_fast.pyx
+++ pyrfm/linear_model/adam_fast.pyx
@@ -26,6 +26,21 @@ cdef inline double proximal(double coef,
         return 0.
 
 
+cdef inline double _pred(double[:] z,
+                         double[:] coef,
+                         double intercept,
+                         double lam1,
+                         double lam2,
+                         double* acc_loss,
+                         Py_ssize_t n_components):
+    cdef double y_pred = 0
+    cdef Py_ssize_t j
+    for j in range(n_components):
+        y_pred += z[j] * coef[j]
+        acc_loss[0] += 0.5*lam2*coef[j]**2 + lam1*fabs(coef[j])
+    return y_pred + intercept
+
+
 cdef double adam_epoch(double[:] coef,
                        double[:] intercept,
                        RowDataset X,
@@ -40,6 +55,7 @@ cdef double adam_epoch(double[:] coef,
                        LossFunction loss,
                        double lam1,
                        double lam2,
+                       double intercept_decay,
                        double eta,
                        double beta1,
                        double beta2,
@@ -89,41 +105,29 @@ cdef double adam_epoch(double[:] coef,
         if mean is not None:
             normalize(z, mean, var, t[0], n_components)
 
-        y_pred = 0
-        norm = 0
-        for j in range(n_components):
-            y_pred += z[j] * coef[j]
-            acc_loss[0] += 0.5*lam2*coef[j]**2 + lam1*fabs(coef[j])
-
-        y_pred += intercept[0]
+        y_pred = _pred(z, coef, intercept[0], lam1, lam2, &acc_loss[0],
+                       n_components)
         acc_loss[0] += loss.loss(y_pred, y[i])
 
         # update parameters
         dloss = loss.dloss(y_pred, y[i])
         eta_t = eta * sqrt(1-beta2**t[0]) / (1-beta1**t[0])
-
         for j in range(n_components):
-            # grad = dloss*z[j]
-            grad = dloss*z[j]+lam2*coef[j]
+            grad = dloss*z[j] + lam2*coef[j]
             mean_grad[j] = beta1*mean_grad[j] + (1-beta1)*grad
             var_grad[j] = beta2*var_grad[j] + (1-beta2)*grad**2
-
-        for j in range(n_components):
-            # m_hat_t = mean_grad[j] / (1-beta1**t[0])
-            # v_hat_t = var_grad[j] / (1-beta2**t[0])
-            # denom = sqrt(v_hat_t) + eps + lam2*eta
             denom = sqrt(var_grad[j]) + eps
-            # coef_new_j = ((sqrt(v_hat_t)+eps)*coef[j] - eta*m_hat_t) / denom
             coef_new_j = coef[j] - eta_t * mean_grad[j] / denom
             viol += fabs(coef[j] - coef_new_j)
             coef[j] = coef_new_j
             coef[j] = proximal(coef_new_j, eta*lam1/sqrt(t[0]))
 
         if fit_intercept:
+            grad = dloss + intercept_decay*intercept[0]
             mean_grad_intercept[0] *= beta1
-            mean_grad_intercept[0] += (1-beta1)*dloss
+            mean_grad_intercept[0] += (1-beta1)*grad
             var_grad_intercept[0] *= beta2
-            var_grad_intercept[0] += (1-beta2)*dloss*dloss
+            var_grad_intercept[0] += (1-beta2)*grad*grad
 
             m_hat_t = mean_grad_intercept[0] / (1-beta1**t[0])
             v_hat_t = var_grad_intercept[0] / (1-beta2**t[0])
@@ -152,6 +156,7 @@ def _adam_fast(double[:] coef,
                LossFunction loss,
                double alpha,
                double l1_ratio,
+               double intercept_decay,
                double eta,
                double beta1,
                double beta2,
@@ -186,7 +191,7 @@ def _adam_fast(double[:] coef,
         viol = adam_epoch(coef, intercept, X, X_array, y, mean_grad,
                           var_grad, mean_grad_intercept,
                           var_grad_intercept, mean, var, loss, lam1,
-                          lam2, eta, beta1, beta2, &t, eps,
+                          lam2, intercept_decay, eta, beta1, beta2, &t, eps,
                           is_sparse, fit_intercept, shuffle, random_state,
                           &acc_loss, transformer, transformer_fast,
                           indices_samples, z)
