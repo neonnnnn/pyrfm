10f72b15dbcf10c63f71f5bf028366d6ddf27bb9
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-10-20 22:25:29 +0900

    Add __all__

--- pyrfm/__init__.py
+++ pyrfm/__init__.py
@@ -1,17 +1,31 @@
-from .random_feature import (RandomSubsetKernel, RandomMaclaurin, RandomKernel,
-                             RandomFourier, SignedCirculantRandomKernel,
+from .random_feature import (RandomSubsetKernel, RandomMaclaurin,
+                             RandomKernel, RandomFourier,
+                             SignedCirculantRandomKernel,
                              TensorSketch, MB, SparseMB, RandomProjection,
                              FastFood, SubsampledRandomHadamard,
-                             SignedCirculantRandomMatrix, CompactRandomFeature,
+                             SignedCirculantRandomMatrix,
+                             CompactRandomFeature,
                              OrthogonalRandomFeature,
                              StructuredOrthogonalRandomFeature)
 from .kernels import (anova, all_subsets, chi_square, hellinger,
                       intersection, anova_fast)
-from .kernels_fast import (_anova, _all_subsets, _chi_square, _intersection,
-                           score)
+from .kernels_fast import (_anova, _all_subsets, _chi_square,
+                           _intersection, score)
 from .linear_model import (AdaGradRegressor, AdaGradClassifier,
                            SparseMBRegressor, SparseMBClassifier,
                            SDCARegressor, SDCAClassifier,
                            AdamRegressor, AdamClassifier,
                            SGDRegressor, SGDClassifier,
                            SAGARegressor, SAGAClassifier)
+
+
+__all__ = ['RandomKernel', 'RandomSubsetKernel', 'RandomFourier',
+           'TensorSketch', 'RandomMaclaurin', 'MB', 'SparseMB',
+           'RandomProjection', 'FastFood', 'CompactRandomFeature',
+           'SubsampledRandomHadamard', 'SignedCirculantRandomMatrix',
+           'SignedCirculantRandomKernel',
+           'OrthogonalRandomFeature', 'StructuredOrthogonalRandomFeature',
+           'AdaGradRegressor', 'AdaGradClassifier', 'AdamRegressor',
+           'AdamClassifier', 'SparseMBRegressor', 'SparseMBClassifier',
+           'SDCARegressor', 'SDCAClassifier', 'SGDClassifier', 'SGDRegressor',
+           'SAGARegressor', 'SAGAClassifier']
