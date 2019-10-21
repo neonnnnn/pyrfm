13d947ca2fdaa8beb614574ce31c80160e80092f
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-10-21 19:36:59 +0900

    Add SubfeatureRandomMaclaurin

--- pyrfm/random_feature/__init__.py
+++ pyrfm/random_feature/__init__.py
@@ -1,6 +1,6 @@
 from .random_fourier import RandomFourier
 from .random_kernel import RandomKernel, SubfeatureRandomKernel
-from .random_maclaurin import RandomMaclaurin
+from .random_maclaurin import RandomMaclaurin, SubfeatureRandomMaclaurin
 from .signed_circulant_random_kernel import SignedCirculantRandomKernel
 from .maji_berg import MB, SparseMB
 from .tensor_sketch import TensorSketch
