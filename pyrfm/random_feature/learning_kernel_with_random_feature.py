1717771c67179866ad86a5b7f354ad132308fa59
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-11-07 21:13:12 +0900

    Add reverse_kl divergence and BaseADMMDivergence

--- pyrfm/random_feature/learning_kernel_with_random_feature.py
+++ pyrfm/random_feature/learning_kernel_with_random_feature.py
@@ -48,7 +48,7 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
         If not None, optimize the regularized objective (+ alpha * divergence).
         If None, optimize the constraied objective (divergence < rho).
 
-    tol : double (default=1e-6)
+    tol : double (default=1e-8)
         Tolerance of stopping criterion.
 
     warm_start : bool (default=False)
@@ -56,32 +56,32 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
 
     max_iter_admm : int (default=10000)
         Maximum number of iterations in the ADMM optimization.
-        This is used if divergence='tv'.
+        This is used if divergence='tv' or 'reverse_kl'.
     
     mu : double (default=10)
         A parameter for the ADMM optimization.
         Larger mu updates the penalty parameter more frequently.
-        This is used if divergence='tv'.
+        This is used if divergence='tv' or 'reverse_kl'.
 
     tau_incr : double (default=2)
         A parameter for the ADMM optimization.
         The penalty parameter updated by multiplying tau_incr.
-        This is used if divergence='tv'.
+        This is used if divergence='tv' or 'reverse_kl'.
 
     tau_decr : double (default=2)
         A parameter for the ADMM optimization.
         The penalty parameter updated by dividing tau_decr.
-        This is used if divergence='tv'.
+        This is used if divergence='tv' or 'reverse_kl'.
         
-    eps_abs : double (default=1e-3)
+    eps_abs : double (default=1e-4)
         A parameter for the ADMM optimization.
         It is used for stopping criterion.
-        This is used if divergence='tv'.
+        This is used if divergence='tv' or 'reverse_kl'.
      
-    eps_rel : double (default=1e-3)
+    eps_rel : double (default=1e-4)
         A parameter for the ADMM optimization.
         It is used for stopping criterion.
-        This is used if divergence='tv'.
+        This is used if divergence='tv' or 'reverse_kl'.
 
     verbose : bool (default=True)
         Verbose mode or not.
@@ -103,9 +103,9 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
 
     """
     def __init__(self, transformer=None, divergence='chi2', rho=1.,
-                 alpha=None, max_iter=100, tol=1e-6, warm_start=False,
+                 alpha=None, max_iter=100, tol=1e-8, warm_start=False,
                  max_iter_admm=10000, mu=10, tau_incr=2, tau_decr=2,
-                 eps_abs=1e-3, eps_rel=1e-3, verbose=True):
+                 eps_abs=1e-4, eps_rel=1e-4, verbose=True):
         self.transformer = transformer
         self.divergence = divergence
         self.max_iter = max_iter
@@ -168,9 +168,15 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
         elif self.divergence == 'squared':
             return Squared(self.rho, self.alpha, self.tol, self.warm_start, 
                            self.max_iter, self.verbose)
+        elif self.divergence == 'reverse_kl':
+            return ReverseKL(self.rho, self.alpha, self.tol, self.warm_start, 
+                             self.max_iter, self.verbose, self.max_iter_admm,
+                             self.mu, self.tau_incr, self.tau_decr,
+                             self.eps_abs, self.eps_rel)
         else:
-            raise ValueError("f={} is not supported now. Use {'chi2'|'kl'|
-                             "'tv'|'squared'}.".format(self.divergence))
+            raise ValueError("f={} is not supported now. Use {'chi2'|'kl'|"
+                             "'tv'|'reverse_kl'|'squared'}."
+                             .format(self.divergence))
 
 
 class BaseDivergence(metaclass=abc.ABCMeta):
@@ -242,6 +248,57 @@ class BaseDivergence(metaclass=abc.ABCMeta):
         return self.divergence(p, q)
 
 
+class BaseADMMDivergence(BaseDivergence):
+    """Base class for 
+    minimizing -p^T v + lam*D(z-1/n, 1/n) + y^T(p-z-1/n) + rho*||p-q-1/n||_2^2/2
+    by ADMM.
+
+    """
+    def __init__(self, rho, alpha, tol, warm_start, max_iter, verbose,
+                 max_iter_admm, mu, tau_incr, tau_decr, eps_abs, eps_rel):
+        super(BaseADMMDivergence, self).__init__(rho, alpha, tol, warm_start, max_iter, verbose)
+        self.max_iter_admm = max_iter_admm
+        self.mu = mu
+        self.tau_incr = tau_incr
+        self.tau_decr = tau_decr
+        self.eps_abs = eps_abs
+        self.eps_rel =  eps_rel
+
+    def _fit(self, p, v, lam):
+        # optimize by ADMM
+        multipler = 1.0
+        n = len(v)
+        z = np.ones(n)/n # augmented parameter
+        y = np.zeros(n) # Lagrangian
+        for _ in range(self.max_iter_admm):
+            # update x
+            p[:] = proj_l1ball(v - y + multipler*z, multipler) / multipler
+            # update z (augmented parameter, x - (z) = 0)
+            z_new = self._optimize_z(p, n, y, multipler, lam)
+            norm_residual_p = sqrt(np.sum((p - z_new)**2))
+            norm_residual_d = sqrt(np.sum(multipler * (z - z_new))**2)
+            z = z_new
+            # update Lagrangian
+            y += multipler * (p - z)
+            # stopping criterion
+            norm_p = sqrt(np.dot(p, p))
+            norm_z = sqrt(np.dot(z, z))
+            norm_y = sqrt(np.dot(y, y))
+            upper_primal = sqrt(n) * self.eps_abs
+            upper_primal += self.eps_rel * max(norm_p, norm_z)
+            upper_dual = sqrt(n) * self.eps_abs
+            upper_dual += self.eps_rel * norm_y
+            if norm_residual_p < upper_primal and norm_residual_d < upper_dual:
+                break
+            if norm_residual_p > norm_residual_d * self.mu:
+                multipler *= self.tau_incr
+            elif norm_residual_d > norm_residual_p * self.mu:
+                multipler /= self.tau_decr
+
+    def eval(self, p, q):
+        return 0.5*np.sum(np.abs(p-q))
+
+
 class Chi2(BaseDivergence):
     def _fit(self, p, v, lam):
         scale = 2*lam*len(v)
@@ -273,59 +330,29 @@ class KL(BaseDivergence):
         return np.dot(np.log(t[t!=0]), p[t!=0])
 
 
-class TV(BaseDivergence):
+class TV(BaseADMMDivergence):
     """Total variation divergence.
 
-    minimizing -p^T v + 0.5 * lam * |z| + y^T(p-z-1/n) + rho*||p-q-1/n||_2^2
-    by ADMM.
-
     """
-    def __init__(self, rho, alpha, tol, warm_start, max_iter, verbose,
-                 max_iter_admm, mu, tau_incr, tau_decr, eps_abs, eps_rel):
-        super(TV, self).__init__(rho, alpha, tol, warm_start, max_iter, verbose)
-        self.max_iter_admm = max_iter_admm
-        self.mu = mu
-        self.tau_incr = tau_incr
-        self.tau_decr = tau_decr
-        self.eps_abs = eps_abs
-        self.eps_rel =  eps_rel
-
-    def _fit(self, p, v, lam):
-        # optimize by ADMM
-        multipler = 1.0
-        n = len(v)
-        z = np.zeros(n) # augmented parameter
-        y = np.zeros(n) # Lagrangian
-        for _ in range(self.max_iter_admm):
-            # update x
-            p[:] = proj_l1ball(v - y + multipler*(z+1./n), multipler) / multipler
-            # update z (augmented parameter, x - (z+1./n) = 0)
-            # apply soft-threasholding
-            z_new = (multipler * (p-1./n) + y) / multipler
-            z_new -= np.sign(z_new) * 1.0*lam / (2*multipler)
-            z_new[np.abs(z_new) <= (1.0*lam / (2*multipler))] = 0.
-            norm_residual_p = sqrt(np.sum((p - z_new)**2))
-            norm_residual_d = sqrt(np.sum(multipler * (z - z_new))**2)
-            z[:] = z_new
-            # update Lagrangian
-            y += multipler * (p - z - 1./n)
-            # stopping criterion
-            norm_p = sqrt(np.dot(p, p))
-            norm_z = sqrt(np.dot(z, z))
-            norm_y = sqrt(np.dot(y, y))
-            upper_primal = sqrt(n) * self.eps_abs
-            upper_primal += self.eps_rel * max(norm_p, norm_z)
-            upper_dual = sqrt(n) * self.eps_abs
-            upper_dual += self.eps_rel *  norm_y
-            if norm_residual_p < upper_primal and norm_residual_d < upper_dual:
-                break
-            if norm_residual_p > norm_residual_d * self.mu:
-                multipler *= self.tau_incr
-            elif norm_residual_d > norm_residual_p * self.mu:
-                multipler /= self.tau_decr
+    def _optimize_z(self, p, n, y, multipler, lam):
+        # apply soft-threasholding
+        z_new = (multipler * (p-1./n) + y) / multipler
+        z_new -= np.sign(z_new) * (1.0*lam / (2*multipler))
+        z_new[np.abs(z_new) <= (1.0*lam / (2*multipler))] = 0
+        return z_new + 1./n
 
     def eval(self, p, q):
         return 0.5*np.sum(np.abs(p-q))
+
+
+class ReverseKL(BaseADMMDivergence):
+    def _optimize_z(self, p, n, y, multipler, lam):
+        a = multipler * n
+        b = n * (y + multipler * p)
+        return (b + np.sqrt(b*b + 4*a*lam)) / (2*a)
+
+    def eval(self, p, q):
+        return np.dot(q, np.log(q) - np.log(p+1e-8))
 
 
 class Squared(BaseDivergence):
