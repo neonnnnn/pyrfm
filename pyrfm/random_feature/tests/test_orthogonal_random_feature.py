c4c58b95d55d33366a8b02482c836baaca3d2cbe
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-11-14 15:34:27 +0900

    Update tests

--- pyrfm/random_feature/tests/test_orthogonal_random_feature.py
+++ pyrfm/random_feature/tests/test_orthogonal_random_feature.py
@@ -13,7 +13,6 @@ X /= np.sum(X, axis=1, keepdims=True)
 Y /= np.sum(Y, axis=1, keepdims=True)
 X_sp = csr_matrix(X)
 
-
 def test_orthogonal_random_feature():
     for gamma, n_components in zip([10, 100], [2048, 4096]):
         # compute exact kernel
@@ -21,6 +20,27 @@ def test_orthogonal_random_feature():
         # approximate kernel mapping
         rf_transform = OrthogonalRandomFeature(n_components=n_components,
                                                gamma=gamma,
+                                               random_state=0)
+        X_trans = rf_transform.fit_transform(X)
+        Y_trans = rf_transform.transform(Y)
+        kernel_approx = np.dot(X_trans, Y_trans.T)
+
+        error = kernel - kernel_approx
+        assert_less_equal(np.abs(np.mean(error)), 0.01)
+        assert_less_equal(np.max(error), 0.1)  # nothing too far off
+        assert_less_equal(np.mean(error), 0.05)  # mean is fairly close
+        # for sparse matrix
+        X_trans_sp = rf_transform.transform(csr_matrix(X))
+        assert_allclose_dense_sparse(X_trans, X_trans_sp)
+
+
+def test_orthogonal_random_feature_use_offset():
+    for gamma, n_components in zip([10, 100], [2048, 4096]):
+        # compute exact kernel
+        kernel = rbf_kernel(X, Y, gamma)
+        # approximate kernel mapping
+        rf_transform = OrthogonalRandomFeature(n_components=n_components,
+                                               gamma=gamma, use_offset=True,
                                                random_state=0)
         X_trans = rf_transform.fit_transform(X)
         Y_trans = rf_transform.transform(Y)
