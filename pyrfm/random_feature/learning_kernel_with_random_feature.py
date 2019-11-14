ceaf5719ccac94aaf92ba135580ae0b22feb4cb9
neonnnnn <atarashi_k@complex.ist.hokudai.ac.jp>
2019-11-14 15:30:20 +0900

    Add scaling argument and remove_bases method for sparse importance_weights_

--- pyrfm/random_feature/learning_kernel_with_random_feature.py
+++ pyrfm/random_feature/learning_kernel_with_random_feature.py
@@ -12,8 +12,11 @@ from scipy.optimize import linprog
 import warnings
 import abc
 from .learning_kernel_with_random_feature_fast import proj_l1ball
+from .learning_kernel_with_random_feature_fast import compute_X_trans_y
+import warnings
 from math import sqrt
 
+EPS = 1e-8
 
 class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
     """ Learnes importance weights for random features by maximizing the 
@@ -36,6 +39,8 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
 
         - 'squared': 0.5*(p-q)^2
 
+        - 'reverse_kl': (- log (p/q))
+
     max_iter : int (default=100)
         Maximum number of iterations.
 
@@ -50,6 +55,11 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
 
     tol : double (default=1e-8)
         Tolerance of stopping criterion.
+
+    scaling : bool (default=True)
+        Whether to scaling the kernel alignment term in objective or not.
+        If True, the kernel alignment term is scaled to [0, 1] by dividing the
+        maximum kernel alignment value.
 
     warm_start : bool (default=False)
         Whether to active warm-start or not.
@@ -120,8 +130,27 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
         self.tau_decr = tau_decr
         self.eps_abs = eps_abs
         self.eps_rel =  eps_rel
+        self.is_removed = False
+        self.n_components = None
 
     def fit(self, X, y):
+        """Fitting the importance weights of each random basis. 
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array, shape (n_samples, )
+            Labels, where n_samples is the number of samples.
+            y must be in {-1, 1}^n_samples.
+
+        Returns
+        -------
+        self : object
+            Returns the transformer.
+        """
         X, y = check_X_y(X, y)
         y_kind = np.unique(y)
         if not (y_kind[0] == -1 and y_kind[1] == 1):
@@ -132,24 +161,84 @@ class LearningKernelwithRandomFeature(BaseEstimator, TransformerMixin):
        
         if self.transformer is None:
             self.transformer = RBFSampler()
+        
         if not (self.warm_start 
                 and hasattr(self.transformer, "random_weights_")):
             self.transformer.fit(X)
-        X_trans = self.transformer.transform(X)
+                
+        if self.is_removed:
+            warnings.warn("some columns of random_weights_ were removed "
+                          "and so transformer is refit.")
+            n_components = self.importance_weights_.shape[0]
+            self.transformer.set_params(n_components=n_components)
+            self.transformer.fit(X)
+            self.is_removed = False
+        
+        if self.n_components is None:
+            self.n_components = self.transformer.n_components
 
+        v = compute_X_trans_y(self.transformer, X, y)
+        v /= np.max(v) # scaling
         if not (self.warm_start and hasattr(self, "importance_weights_")):
-            self.importance_weights_ = np.zeros(X_trans.shape[1])
-
-        scale = np.sqrt(X_trans.shape[1])
-        v = safe_sparse_dot(y, X_trans*scale, dense_output=True)**2
-
+            self.importance_weights_ = np.zeros(v.shape[0])
+    
         self.divergence_.fit(self.importance_weights_, v)
         return self
     
     def transform(self, X):
-        X_transform = self.transformer.transform(X)
-        X_transform *= np.sqrt(X_transform.shape[1]) 
-        return X_transform * np.sqrt(self.importance_weights_)
+        """Apply the approximate feature map to X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            New data, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        Returns
+        -------
+        X_new : array-like, shape (n_samples, n_components)
+        """
+        check_is_fitted(self, "importance_weights_")
+        X_trans = self.transformer.transform(X)
+        X_trans *= np.sqrt(X_trans.shape[1])
+        if self.is_removed:
+            indices = np.nonzero(self.importance_weights_)[0]
+            return X_trans * np.sqrt(self.importance_weights_[indices])
+        else:
+            return X_trans * np.sqrt(self.importance_weights_)
+    
+    def remove_bases(self):
+        """Remove the useless random bases according to importance_weights_.
+        
+        Parameters
+        ----------
+
+        Returns
+        -------
+        self : bool
+            Whether to remove bases or not.
+        """
+        check_is_fitted(self, "importance_weights_")
+        remove_indices = np.where(self.importance_weights_ == 0)[0]
+        
+        flag = False
+        if self.is_removed:
+            warnings.warn("Random bases have already been removed.")
+        elif len(remove_indices) == 0:
+            if self.verbose:
+                print("importance_weights has no zero values. "
+                      "Bases are not removed.")
+        elif hasattr(self.transformer, '_remove_bases'):
+            if self.transformer._remove_bases(remove_indices):
+                self.is_removed = True
+                self.n_components = len(self.importance_weights_.nonzero())
+                flag = True
+            else:
+                if self.verbose:
+                    print("Bases are not removed.")
+        else:
+            warnings.warn("transformer does not have _remove_bases method.")
+        return flag
 
     def _get_divergence(self):
         if self.divergence == 'chi2':
@@ -327,7 +416,7 @@ class KL(BaseDivergence):
 
     def eval(self, p, q):
         t = p/q
-        return np.dot(np.log(t[t!=0]), p[t!=0])
+        return np.dot(np.log(t[p!=0]), p[p!=0])
 
 
 class TV(BaseADMMDivergence):
@@ -352,7 +441,7 @@ class ReverseKL(BaseADMMDivergence):
         return (b + np.sqrt(b*b + 4*a*lam)) / (2*a)
 
     def eval(self, p, q):
-        return np.dot(q, np.log(q) - np.log(p+1e-8))
+        return np.dot(q, np.log(q) - np.log(p+EPS))
 
 
 class Squared(BaseDivergence):
